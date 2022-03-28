645e24573215c867321905555c2da8fa7d03f808
bablikumari2 <anandbabli96@gmail.com>
2022-03-28 16:49:06 +0530

    meet

--- src/Components/Routers/Routers.jsx
+++ src/Components/Routers/Routers.jsx
@@ -12,11 +12,18 @@ export const Routers = () => {
         <Routes>
             <Route  path="/" element={<Home />} />
             <Route exact path="/loginsignup" element = {<LoginSignUp/>} />
-            <Route exact path="/meetup" element = {<AddMeetup/>} />
+            <Route exact path="/addmeetup" element = {<AddMeetup/>} />
+
             <Route exact path="/events" element = {<Event />} />
             <Route path="*" element = {<NotFound/>} />
+         <Route path="/meetup/:id" element={<AddMeetup/>} />
             {/* Create other routes here: loginsignup, meetup, addmeetup and 404 */}
             {/* meetup route should be dynamic */}
         </Routes>
     </>);
 }
+// / for home page
+// /loginsignup for login and signup
+// /meetup/:id for showing meetup details page with ID
+// /addmeetup for adding/creating meetups
+// * for 404 page. (using * as path will show 404 page automatically)
